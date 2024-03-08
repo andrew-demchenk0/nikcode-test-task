f5751bd7458e489627a49b3f1d7de522a482a46a
Andrii Demchenko <2andrew.demchenko@gmail.com>
2024-03-08 20:03:34 +0200

    update service

--- src/services/offerService.ts
+++ src/services/offerService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://deayloop.backend.test.starway.agency:8002/api';
+const API_URL = 'http://deayloop.backend.test.starway.agency:8002/api';
 
 const fetchAllOffers = async () => {
 	try {
